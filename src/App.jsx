444de39a35ea95239396db040531861cea11c915
Vaishali <bvaishali382@gmail.com>
2026-03-27 13:16:01 +0530

    Removed some irrelevant files

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,8 @@ import Login from './Pages/Login.jsx';
 import Home from "./Pages/Home.jsx";
 import Need from "./Pages/Need.jsx";
 import Start from "./Pages/Start.jsx";
-import RideStart from "./Pages/RideStart.jsx";
-import Test from "./Pages/test.jsx";
 import Match from "./Pages/Match.jsx";
-import Distance from  './Components/Distance.jsx';
+
 
 
 const App = () => {
@@ -21,10 +19,9 @@ const App = () => {
             <Route path = '/register' element = { <Register /> } />
             <Route path = '/need' element = { <Need /> } />
             <Route path = '/start' element = { <Start />} />
-            {/* <Route path = '/ride-start' element = { <RideStart />} /> */}
             <Route path = '/test' element = { <Test /> } /> 
             <Route path = '/match' element = { <Match /> } />
-            <Route path = '/distance' element = { <Distance />} />
+
           </Routes>
       </div>
      
